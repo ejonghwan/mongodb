5a2db3611da307fb3124fa7789562104bedb95db
ejonghwan <sun87-1@daum.net>
2021-12-20 22:36:40 +0900

    쿼리 하나씩 배워보자

--- back/app.js
+++ back/app.js
@@ -1,18 +1,85 @@
 import express from 'express';
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
 
 const app = express();
+app.use(express.json())
+dotenv.config()
 
 
 
+import User from './models/User.js'
 
-app.get('/test', (req, res) => {
-    res.send('hoihohoho')
-})
 
-app.get('/user', (req, res) => {
+const server = async () => {
+    try {
+        const dbInfo = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@mongostudy.5g49u.mongodb.net/${process.env.DB_TITLE}?retryWrites=true&w=majority`
+        await mongoose.connect(dbInfo)
+        .then(result => console.log('몽고디비 연결성공'))
     
-})
 
-app.listen(3000, () => {
-    console.log('listen')
-})
+        // model
+
+        // const user = new User({
+        //     username: 'jonghwan222',
+        //     name: { first: 'lee222', last: 'jonghwan222' },
+        //     age: 30,
+        //     email: 'jjongrrr@naver.com222'
+        // })
+    
+        // user.save();
+
+
+        app.post('/api/user/signup', async (req, res) => {
+            try {
+                console.log(req.body)
+                let { username, name } = req.body; // 클라이언트 입력오류는 여기서 체크해줌
+
+                if(!username) return res.status(400).send({ err: 'username is required' })
+                if(!name || !name.first || !name.last) return res.status(400).send({ err: 'name is required' })
+
+
+                const user = new User(req.body)
+                await user.save();
+                res.status(200).send(user)
+            } catch(err) {
+                console.log(err)
+                return res.status(500).json({ err: err.message })
+            }
+            
+        })
+
+
+
+        app.get('/api/user/alluser', async (req, res) => {
+            try {
+                const user = await User.find({});
+                res.status(200).json({ user })
+            } catch(err) {
+                console.log(err)
+            }
+            
+        })
+        
+
+
+    
+        // app.get('/test', (req, res) => {
+        //     res.send('hoihohoho')
+        // })
+    
+        // app.get('/user', (req, res) => {
+            
+        // })
+    
+        app.listen(3000, () => {
+            console.log('listen')
+        })
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+
+server();
---
+++ back/models/User.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose'
+
+// 스케마는 첫번째 인자로 저장할 데이터들 row를 받고 두번째 인자로 옵션들을 받음. 둘다 객체로
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    name: {
+        first: { type: String, required: true,},
+        last: { type: String, required: true, }
+    },
+    age: Number,
+    email: String,
+}, {
+    timestamps: true, //생성한 시간을 만들어줌, 업데이트할 때마다 업데이트키를 생성해줌 
+})
+
+const User = mongoose.model('user', UserSchema)
+export default User;
